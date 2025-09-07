58d6c1d1cdb765ab5d16f8f893df46c4e1533a30
michalsmola <michalsmoladev@gmail.com>
2025-09-07 21:29:58 +0200

    init

---
+++ src/main/java/me/michalsmoladev/pokemondeckvault/PokemonDeckVaultApplication.java
@@ -0,0 +1,13 @@
+package me.michalsmoladev.pokemondeckvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonDeckVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonDeckVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/michalsmoladev/pokemondeckvault/PokemonDeckVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package me.michalsmoladev.pokemondeckvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonDeckVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
